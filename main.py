5a6be6e09121ea711adaa1492d9ec435ff29c8f3
Dario <dariosce99@gmail.com>
2026-01-08 11:41:59 -0500

    Fixed models and API

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from fastapi import FastAPI
+from sqlmodel import select
 
 from db import SessionDep, create_all_tables
 from models import Customer, CustomerCreate, Invoice, Transaction
@@ -11,27 +14,32 @@ async def root():
     return {"message": "Hello, World"}
 
 
-db_customer: list[Customer] = []
-
-
 @app.post("/customer", response_model=Customer)
-async def create_customer(customer_data: CustomerCreate, session=SessionDep):
+async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump())
-    customer.id = len(db_customer)
-    db_customer.append(customer)
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
     return customer
 
 
-@app.get("/customer")
-async def get_customer():
-    return db_customer
+@app.get("/customer", response_model=List[Customer])
+async def get_customer(session: SessionDep):
+    customers = session.exec(select(Customer)).all()
+    return customers
 
 
 @app.post("/transactions")
-async def create_transaction(transaction_data: Transaction, session=SessionDep):
+async def create_transaction(transaction_data: Transaction, session: SessionDep):
+    session.add(transaction_data)
+    session.commit()
+    session.refresh(transaction_data)
     return transaction_data
 
 
 @app.post("/invoices")
-async def create_invoice(invoices_data: Invoice, session=SessionDep):
+async def create_invoice(invoices_data: Invoice, session: SessionDep):
+    session.add(invoices_data)
+    session.commit()
+    session.refresh(invoices_data)
     return invoices_data
